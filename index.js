ce78ffea12cee806701db36e9b65de7fda373f63
Yumana <yumanuralfath2@gmail.com>
2024-03-05 11:41:32 +0700

    Swagger init  implementation with morgan for logging server

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ import db from "./config/database.js";
 import dotenv from "dotenv";
 import session from "express-session";
 import sequelizeStore from "connect-session-sequelize";
-import Movierouter from "./router/MovieRoute.js";
-import UserRouter from "./router/UserRouter.js";
-import AuthRouter from "./router/AuthRoute.js";
+import Movierouter from "./routes/MovieRoute.js";
+import UserRouter from "./routes/UserRouter.js";
+import AuthRouter from "./routes/AuthRoute.js";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import morgan from "morgan";
 dotenv.config();
 
 const port = process.env.PORT || 2070;
@@ -19,13 +22,35 @@ const store = new sessionStore({
   expiration: 1 * 60 * 60 * 1000,
 });
 
+//Make Documentation With Swagger
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Express API with Swagger",
+      version: "1.0.0",
+      description:
+        "this is a simple CRUD APP With Restful API with Express and documented by Swagger",
+    },
+    servers: [
+      {
+        url: "http://localhost:2070",
+      },
+    ],
+  },
+  apis: ["./routes/*"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
 //Database Check
-try {
-  await db.authenticate();
-  console.log("connection established successfully");
-} catch (error) {
-  console.error("unable to connect to database");
-}
+// try {
+//   await db.authenticate();
+//   console.log("connection established successfully");
+// } catch (error) {
+//   console.error("unable to connect to database");
+// }
 
 //middleware
 app.use(
@@ -39,6 +64,7 @@ app.use(
     },
   })
 );
+app.use(morgan("common"));
 app.use(express.json());
 app.use(AuthRouter);
 app.use(Movierouter);
@@ -47,5 +73,5 @@ app.use(UserRouter);
 // store.sync(); //sync with store session
 
 app.listen(port, () => {
-  console.log("serve running on http://localhost:" + port);
+  console.log("server running on http://localhost:" + port);
 });
